c9c71171009c594a0d945d3268cca81079029618
Ritik pandey <kushagarakp10@gmail.com>
2024-04-07 12:02:53 +0530

    fixed auth bugs

--- controller/authController.js
+++ controller/authController.js
@@ -36,7 +36,7 @@ async function loginController(req, res) {
     const existingUser = await User.findOne({ email });
 
     if (!existingUser) {
-      res.json({
+    return  res.status(400).json({
         msg: "Not an existing user ,register first",
       });
     }
@@ -48,52 +48,65 @@ async function loginController(req, res) {
 
     const pass = await becrypt.compare(password, existingUser.password);
     if (!pass) {
-      res.json({
+      return res.status(403).json({
         msg: "invalid password",
       });
     }
     const token = await jwt.sign({ existingUser }, "secret");
-    return res.json({
-      msg: "Login sucessfull",
-      token,
-      user: user,
-    });
+
+    if (token) {
+      return res.status(200).json({
+        msg: "Login sucessfull",
+        token,
+        user: user,
+      });
+    }
   } catch (err) {
-    res.json({
+   return res.status(403).json({
       msg: err.message,
     });
   }
 }
 
 async function updatePasswordController(req, res) {
-  const { password, confirmpassword } = req.body;
-  const id = req.user._id;
+  const { password, confirmPassword } = req.body;
+  const _id = req.user._id;
   try {
-    if (password == confirmpassword) {
-      const hashedpassword = await becrypt.hash(password, 10);
+    if (password === confirmPassword) {
+      const hashedPassword = await becrypt.hash(password, 10);
 
-      const user = await User.findByIdAndUpdate(id, {
-        password:hashedpassword,
+      const user = await User.findByIdAndUpdate(_id, {
+        password: hashedPassword,
       });
 
       if (user) {
         res.json({
-          msg: "Password changed Sucessfully",
+          msg: "Password changed successfully",
+        });
+      } else {
+        res.status(404).json({
+          msg: "User not found",
         });
       }
-    }
-    else{
-      res.json({
+    } else {
+      res.status(400).json({
         msg: "Passwords do not match",
       });
     }
   } catch (err) {
-    res.json({ msg: err.message });
+    res.status(500).json({ msg: err.message });
   }
 }
 
 async function forgotPasswordController(req, res) {
   const { email } = req.body;
+  const user = await User.findById(req.user._id);
+  if (user.email === email) {
+  } else {
+    res.json({
+      msg: "Email not found",
+    });
+  }
 }
 
 module.exports = {
